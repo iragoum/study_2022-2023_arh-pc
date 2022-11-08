c6fe7a6ca1fc9ee8c023288e96dfb1c1a6da7c76
iragoum <1032215692@pfur.ru>
2022-11-08 03:59:12 +0300

    feat(main): add files lab-5

---
+++ labs/lab05/lab05/lab5.asm
@@ -0,0 +1,18 @@
+SECTION .data
+	nfname:	DB 'Mougari Abderrahim',10 
+	
+	nfnameLen: EQU $-nfname
+
+SECTION .text
+	GLOBAL _start
+	
+_start:
+mov eax,4
+mov ebx,1
+mov ecx,nfname
+mov edx,nfnameLen
+int 80h
+
+mov eax,1
+mov ebx,0
+int 80h
